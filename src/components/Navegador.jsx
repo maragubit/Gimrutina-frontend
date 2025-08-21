e5c7bc876646ec75290aab646f3f32bf804de5d8
maragubit <maragubit@gmail.com>
2025-08-21 11:08:33 +0200

    navbar collapse close

--- src/components/Navegador.jsx
+++ src/components/Navegador.jsx
@@ -18,7 +18,7 @@ const[profile,setProfile]=useState();
 const [error,setError]=useState();
 const[loadData,setLoadData]=useState(true);
 const [refresh,setRefresh]=useState(0);
-
+const [expanded, setExpanded] = useState(false);
 useEffect(()=>{
   const fetchProfile=async()=>{
     try{
@@ -36,31 +36,31 @@ useEffect(()=>setRefresh(prev => prev+1),[profile])
 
   return (
     <>
-      <Navbar  expand="lg" bg="light" className="tm-top-bar" id="tm-top-bar">
+      <Navbar expanded={expanded}  expand="lg" bg="light" className="tm-top-bar" id="tm-top-bar">
         <Container fluid>
           <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
             <img src={logo} width="50" alt="Site logo" className="mr-2" />
             GIMRUTINA
           </Navbar.Brand>
 
-          <Navbar.Toggle aria-controls="mainNav" />
+          <Navbar.Toggle aria-controls="mainNav" onClick={() => setExpanded(expanded ? false : "expanded")} />
           <Navbar.Collapse id="mainNav">
             <Nav className="ml-auto mr-4" activeKey={location.pathname} style={{fontSize:"15px"}}>
               <Nav.Link eventKey="/" as={Link} to="/">Home</Nav.Link>
               {isAuth ? (
                 <>
-                  <Nav.Link eventKey="/rutinas" as={Link} to="rutinas">Rutinas</Nav.Link>
-                  <Nav.Link eventKey="/ejercicios" as={Link} to="/ejercicios">Ejercicios</Nav.Link>
-                  <Nav.Link eventKey="/entrenamientos/create" as={Link} to="/entrenamientos/create">Entrenar</Nav.Link>
-                  <Nav.Link eventKey="/gimnasios" as={Link} to="/gimnasios">Gimnasios</Nav.Link>
-                  <Nav.Link eventKey="/profile" as={Link} to="profile" className="d-flex justify-content-end ml-5 mr-2">
+                  <Nav.Link eventKey="/rutinas" as={Link} to="rutinas"  onClick={() => setExpanded(false)}>Rutinas</Nav.Link>
+                  <Nav.Link eventKey="/ejercicios" as={Link} to="/ejercicios"  onClick={() => setExpanded(false)}>Ejercicios</Nav.Link>
+                  <Nav.Link eventKey="/entrenamientos/create" as={Link} to="/entrenamientos/create"  onClick={() => setExpanded(false)}>Entrenar</Nav.Link>
+                  <Nav.Link eventKey="/gimnasios" as={Link} to="/gimnasios"  onClick={() => setExpanded(false)}>Gimnasios</Nav.Link>
+                  <Nav.Link eventKey="/profile" as={Link} to="profile" className="d-flex justify-content-end ml-5 mr-2"  onClick={() => setExpanded(false)}>
                     <Card style={{width:"45px", border:"0"}}>
                       {!loadData && profile.image ? <CardImg src={profile.image} style={{borderRadius:"200px"}}/>:<CardImg src={profileIcon}/>}
                     </Card>
                   </Nav.Link>
                 </>
               ) : (
-                <Nav.Link as={Link} to="/login">Login/Register</Nav.Link>
+                <Nav.Link as={Link} to="/login"  onClick={() => setExpanded(false)}>Login/Register</Nav.Link>
               )}
             </Nav>
           </Navbar.Collapse>
